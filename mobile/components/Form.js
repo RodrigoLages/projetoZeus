0afba6b65a023564aa54722d6e8056cbf93e0eeb
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-09-14 11:02:36 -0300

    Form text input

--- mobile/components/Form.js
+++ mobile/components/Form.js
@@ -11,8 +11,24 @@ import {
 export default function Form() {
   return (
     <View style={styles.container}>
-      <Text>Form!</Text>
-      <StatusBar style="light" />
+      <StatusBar style="dark" />
+      <Text style={styles.title}>Adicionar novo gasto</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Quanto você gastou"
+          keyboardType={"numeric"}
+          clearButtonMode="always"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Observações (opcional)"
+          clearButtonMode="always"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -22,6 +38,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "hsl(250, 65%, 88%)",
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 50,
+  },
+  inputContainer: {
+    flex: 1,
+    marginTop: 30,
+    width: "90%",
+    padding: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    alignItems: "stretch",
+    backgroundColor: "#fff",
+  },
+  input: {
+    marginTop: 10,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    fontSize: 16,
+    alignItems: "stretch",
+  },
+  button: {
+    marginTop: 10,
+    height: 60,
+    backgroundColor: "hsl(250, 36%, 57%)",
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    fontSize: 16,
+    alignItems: "center",
     justifyContent: "center",
+    elevation: 20,
+    shadowOpacity: 20,
+    shadowColor: "#ccc",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
